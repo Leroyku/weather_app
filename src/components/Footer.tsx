10eda29899e47feec5e1d4a9aa0645c6346834c3
Leroyku <71512872+Leroyku@users.noreply.github.com>
2023-10-15 18:50:31 +0300

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <div style={{ height: '250px', width: '100%', backgroundColor: '#2c2c2c' }}>Footer</div>;
-};
-
-export default Footer;
